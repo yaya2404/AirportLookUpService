3ecaa68b49cae96fe69bfd9b2b003f0599cbfd4b
Matthew Ya <matthew_ya@yahoo.com>
2016-10-14 23:19:24 -0400

    servers can now run on diffrent rmiregistries
    Initially I assumed the PlaceServer and AirportServer would run on the
    same rmiregistry (host and port number). Now I modified the client code
    so that the AirportServer and Placeserver have the option of running on
    two different rmiregistries.

--- AirportLookUpService/Client.java
+++ AirportLookUpService/Client.java
@@ -6,42 +6,41 @@ import PlaceData.PlaceDataProto.*;
 
 public class Client  {
     public static void main(String args[]) {
-		String host = "localhost";
+		String ahost = "localhost";
+		String phost = "localhost";
 		String city = "";
 		String state = "";
-		int port = 1099;
+		int aport = 1099, pport = 1099;
+		
         try {
-            if (args.length < 2 && args.length > 6) {
-                System.err.println("usage: java Client -h rmiregistryserver -p port city state ... \n");
+            if (args.length < 2) {
+                System.err.println("usage: java Client -ah airportserver -ap airportserverport -ph placeserverhost -pp placeserverport city state ... \n");
                 System.exit(1);
             }
 			
 			String airporturl;
 			String placeurl;
-			if(args[0].equals("-h")){
-				host = args[1];
-				if(args[2].equals("-p")){
-					port = Integer.parseInt(args[3]);
-					city = args[4];
-					state = args[5];
-				}else{
-					city = args[2];
-					state = args[3];
+			int i = 0;
+			if(args.length > 2){
+				for(i = 0; i < args.length - 2; i+=2){
+					if(args[i].equals("-ah")){
+						ahost = args[i+1];
+					}else if(args[i].equals("-ap")){
+						aport = Integer.parseInt(args[i+1]);
+					}else if(args[i].equals("-ph")){
+						phost = args[i+1];
+					}else if(args[i].equals("-pp")){
+						pport = Integer.parseInt(args[i+1]);
+					}
 				}
-
-			}else if(args[0].equals("-p")){
-				port = Integer.parseInt(args[1]);
-				city = args[2];
-				state = args[3];
-			}else{
-				city = args[0];
-				state = args[1];
 			}
-			airporturl = "//" + host + ":" + port + "/Airports";
-			placeurl = "//" + host + ":" + port + "/Places";
+			city = args[i];
+			state = args[i+1];
+			airporturl = "//" + ahost + ":" + aport + "/Airports";
+			placeurl = "//" + phost + ":" + pport + "/Places";
+			
 			AirportInterface airport = (AirportInterface)Naming.lookup(airporturl);
 			PlaceInterface place = (PlaceInterface)Naming.lookup(placeurl);
-			
 			Place p = place.findPlace(city, state);
 			System.out.println(p.getName() + ", " + p.getState() + ": " + p.getLat() + ", " + p.getLon());
 			String[] a = airport.findAirport(p.getLat(), p.getLon());
@@ -50,18 +49,19 @@ public class Client  {
 			System.out.println(a[2]);
 			System.out.println(a[3]);
 			System.out.println(a[4]);
+		}catch(ArrayIndexOutOfBoundsException c){
+			System.out.println("ArrayIndexOutOfBoundsException: you must have forgotten the city or state.");
+		}catch(RemoteException b){
+			System.out.println("RemoteException: remote method findPlace() or findAirport() failed.");
 		}catch(NotBoundException a){
-			System.out.println("NotBoundException: could not connect to host " + host + " on the port number " + port);
+			System.out.println("NotBoundException: could not connect to host on given port number");
 			System.exit(1);
 		}catch(NullPointerException r){
 			System.out.println("Could not find city/state or could not gather nearest airports");
 			System.exit(1);
-		}catch(Exception e) {
+		}catch(Exception e){
             System.out.println("Client exception: " + e);
-			e.printStackTrace();
 			System.exit(1);
         }
-
-		
     }
 }
